1a638028eae9d45528d96ed099314557f7b1b49a
maruf <30905603+mahphujalam@users.noreply.github.com>
2019-04-25 19:36:02 +0530

    Add files via upload

---
+++ copy.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+cd='\033[1;29m'
+de='\033[1;30m'
+ef='\033[1;31m'
+a='\033[1;32m'
+b='\033[1;33m'
+c='\033[1;34m'
+d='\033[1;35m'
+e='\033[1;36m'
+
+w='\033[1;91m'
+x='\033[1;92m'
+y='\033[1;93m'
+z='\033[1;94m'
+aa='\033[1;95m'
+bb='\033[1;96m'
+
+echo " "
+echo " "
+echo "$a Mixplore.apk copy ho rha hai tumhara file manager me $a"
+echo " "
+echo " " cp Mixplore.apk /sdcard
+chmod 777 *
+echo " "
+echo " "
+echo "$b Successfully copy ho gya $b"
